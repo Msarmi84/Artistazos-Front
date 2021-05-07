73dd119e5dfe70bf7c36a00b85005d1b93057c25
Miriam S <miriamogon@outlook.com>
2021-05-07 22:02:25 +0200

    all-products component

---
+++ src/app/models/buscador-producto.ts
@@ -0,0 +1,9 @@
+import { ProductCategory } from "./enums/product-category.enum";
+
+export interface BuscadorProducto{
+    name: string;
+    category: ProductCategory;    
+    artist: string;
+    minPrice: number;
+    maxPrice:number;
+}

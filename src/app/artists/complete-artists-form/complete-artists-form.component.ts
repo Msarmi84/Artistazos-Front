167fdef3ee004491a5de9fe65e5fc5452c4523e0
PabloVerdejo1993 <pablitovva10@gmail.com>
2021-05-13 14:28:17 +0200

    select corregidos

--- src/app/artists/complete-artists-form/complete-artists-form.component.ts
+++ src/app/artists/complete-artists-form/complete-artists-form.component.ts
@@ -17,6 +17,13 @@ export class CompleteArtistsFormComponent implements OnInit {
   imgPreview = 'assets/images/logonofoto.png';
   imageFile: File;
   imageUrl = environment.baseUrl + 'images/';
+
+  locations: string [] = ['Álava/Araba','Albacete','Alicante','Asturias','Ávila','Badajoz','Baleares',
+  'Barcelona','Burgos','Cáceres','Cádiz','Cantabria','Castellón','Ceuta','Ciudad Real','Córdoba',
+'Cuenca','Gerona/Girona','Granada','Guadalajara','Guipúzcoa/Gipuzkoa','Huelva','Huesca','Jaén',
+'La Coruña/A Coruña','La Rioja','Las Palmas','León','Lérida/Lleida','Lugo','Madrid','Málaga','Melilla',
+'Murcia','Navarra','Orense/Ourense','Palencia','Pontevedra','Salamanca','Segovia','Sevilla','Soria',
+'Tarragona','Tenerife','Teruel','Toledo','Valencia','Valladolid','Vizcaya/Bizkaia','Zamora','Zaragoza'];
   
 
   @Output() formSubmitted = new EventEmitter<FormData>();

476c4dee181d55eb5555ee448a874dd648819c6c
Ernesto Valiente <janet.rodriguez@yahoo.es>
2021-05-17 20:38:44 +0200

    token decoder helper

--- src/app/_helpers/tokenHelper.ts
+++ src/app/_helpers/tokenHelper.ts
@@ -1,6 +1,6 @@
 import { JwtHelperService } from "@auth0/angular-jwt";
 
-export const getUserFromToken = (): string => {
+export const getUserOutOfToken = (): string => {
     const tokenHelper = new JwtHelperService();
     const token = localStorage.getItem('AP_TKN');
     const decodedToken = tokenHelper.decodeToken(token);

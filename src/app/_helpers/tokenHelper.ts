eff877f627c04be94c749230a063b271a7782058
Ernesto Valiente <janet.rodriguez@yahoo.es>
2021-05-17 20:40:47 +0200

    cambio de nombre

--- src/app/_helpers/tokenHelper.ts
+++ src/app/_helpers/tokenHelper.ts
@@ -1,6 +1,6 @@
 import { JwtHelperService } from "@auth0/angular-jwt";
 
-export const getUserOutOfToken = (): string => {
+export const getUserFromToken = (): string => {
     const tokenHelper = new JwtHelperService();
     const token = localStorage.getItem('AP_TKN');
     const decodedToken = tokenHelper.decodeToken(token);

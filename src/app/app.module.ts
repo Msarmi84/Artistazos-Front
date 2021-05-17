89d2f8236d4a0e022adb6f71646da02e719938d0
PabloVerdejo1993 <pablitovva10@gmail.com>
2021-05-17 13:59:19 +0200

    modal de productos y estilo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ArtistsFormUpdateComponent } from './artists/artists-form-update/artists-form-update.component';
+import { ProductsModalComponent } from './products/products-modal/products-modal.component';
 
 
 
@@ -56,7 +57,8 @@ import { ArtistsFormUpdateComponent } from './artists/artists-form-update/artist
     ProductsFormUpdateComponent,
     CartComponent,
     ValidCredentialsComponent,
-    InfoComponent
+    InfoComponent,
+    ProductsModalComponent
 
   ],
   imports: [
---
+++ src/app/products/products-modal/products-modal.component.ts
@@ -0,0 +1,22 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Product } from 'src/app/models/product';
+
+@Component({
+  selector: 'app-products-modal',
+  templateUrl: './products-modal.component.html',
+  styleUrls: ['./products-modal.component.scss']
+})
+export class ProductsModalComponent implements OnInit {
+  product: Product;
+  productService: any;
+
+  constructor(@Inject(MAT_DIALOG_DATA) private data?: Product) { }
+
+  ngOnInit(): void {
+    if (this.data){
+      this.product = this.data;
+    }
+  }
+
+}
